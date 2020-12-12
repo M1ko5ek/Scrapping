21954ebbfb3c7d366edd10a14632a1d577f6a30b
M1ko5ek <mikolajlempart@gmail.com>
2020-12-12 16:01:17 +0100

    click button to scrap steam + display info about title and price

--- app/src/main/java/pl/pwr/s241936/steamscrappingtest/MainActivity.java
+++ app/src/main/java/pl/pwr/s241936/steamscrappingtest/MainActivity.java
@@ -1,23 +1,17 @@
 package pl.pwr.s241936.steamscrappingtest;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,36 +21,29 @@ public class MainActivity extends AppCompatActivity {
         final TextView info = findViewById(R.id.textView);
         final Button button = findViewById(R.id.button);
 
-        new Thread(new Runnable() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                try {
-                    Document doc = Jsoup.connect("https://store.steampowered.com/search/?term=Wolcen").get();
-                    Elements el = doc.select("#search_resultsRows");
-                    Element e = el.select("a").first();
-                    //System.out.println(e);
-                    String e0 = el.select("span.title").first().text();
-                    String e1 = e.select("div[class=col search_price  responsive_secondrow]").text();
-                    String e2 = e.select("div[class=col search_price discounted responsive_secondrow]").text();
-                    System.out.println(e0);
-                    System.out.println(e1);
-                    System.out.println(e2);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                new Thread(new Runnable() {
                     @Override
-                    public void onClick(View view) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                System.out.println("TUrururu !!!!");
-
-                            }
-                        });
+                    public void run() {
+                        try {
+                            Document doc = Jsoup.connect("https://store.steampowered.com/search/?term=Wolcen").get();
+                            Elements el = doc.select("#search_resultsRows");
+                            Element e = el.select("a").first();
+                            String e0 = el.select("span.title").first().text();
+                            String e1 = e.select("div[class=col search_price  responsive_secondrow]").text();
+                            String e2 = e.select("div[class=col search_price discounted responsive_secondrow]").text();
+                            System.out.println(e0);
+                            System.out.println(e1);
+                            System.out.println(e2);
+                            info.setText(e0 + e1 + e2);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
-                });
+                }).start();
             }
-        }).start();
+        });
     }
 }
