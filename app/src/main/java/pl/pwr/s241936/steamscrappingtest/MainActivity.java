836d6bb8edbaf7a5790e5eb4fb2ec617d4c2aef5
M1ko5ek <mikolajlempart@gmail.com>
2020-12-14 16:10:00 +0100

    get game name from usser

--- app/src/main/java/pl/pwr/s241936/steamscrappingtest/MainActivity.java
+++ app/src/main/java/pl/pwr/s241936/steamscrappingtest/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -13,22 +14,28 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText gameName;
+    private String name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         final TextView info = findViewById(R.id.textView);
         final Button button = findViewById(R.id.button);
+        gameName = (EditText)findViewById(R.id.editText);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                name = MainActivity.this.gameName.getText().toString();
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
                         try {
-                            Document doc = Jsoup.connect("https://store.steampowered.com/search/?term=Wolcen").get();
+
+                            Document doc = Jsoup.connect("https://store.steampowered.com/search/?term=" + name).get();
                             Elements el = doc.select("#search_resultsRows");
                             Element e = el.select("a").first();
                             String e0 = el.select("span.title").first().text();
