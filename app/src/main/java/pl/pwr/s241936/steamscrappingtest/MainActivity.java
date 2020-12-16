4fe7d7db24d9d3a9c744a41fee04065d3febebe2
M1ko5ek <mikolajlempart@gmail.com>
2020-12-16 18:41:25 +0100

    search title + Wrong title ? -> searching exact title

--- app/src/main/java/pl/pwr/s241936/steamscrappingtest/MainActivity.java
+++ app/src/main/java/pl/pwr/s241936/steamscrappingtest/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView info;
     private Button button;
     private EditText gameName;
+    private TextView wrongName;
     private String name;
 
     @Override
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         info = (TextView)findViewById(R.id.textView);
         button = (Button)findViewById(R.id.button);
         gameName = (EditText)findViewById(R.id.editText);
+        wrongName = (TextView)findViewById(R.id.wrongName);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,27 +38,11 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            Document doc = Jsoup.connect("https://store.steampowered.com/search/?term=" + name + "&category1=998").get();
+                            final Document doc = Jsoup.connect("https://store.steampowered.com/search/?term=" + name + "&category1=998").get();
                             String result = doc.select("div[class=search_results_count]").text();
 
 
-                            Elements el = doc.select("#search_results");
-                            Elements elements = el.select("a");
-                            boolean found = false;
-                            for (Element element : elements){
-                                String title = element.select("span.title").text();
-                                if (name.toUpperCase().equals(title.toUpperCase())){
-                                    String price = element.select("div[class=col search_price  responsive_secondrow]").text();
-                                    String discountedPrice = element.select("div[class=col search_price discounted responsive_secondrow]").text();
-                                    info.setText(title + " " + price + " " + discountedPrice);
-                                    found = true;
-                                }
-                            }
-                            if (found == false){
-                                info.setText("can't find game with this title");
-                            }
-
-                            /*if (result.equals("0 results match your search.")){
+                            if (result.equals("0 results match your search.")){
                                 info.setText("can't find game with this title");
                             } else {
                                 Elements el = doc.select("#search_results");
@@ -64,7 +51,37 @@ public class MainActivity extends AppCompatActivity {
                                 String price = firstElement.select("div[class=col search_price  responsive_secondrow]").text();
                                 String discountedPrice = firstElement.select("div[class=col search_price discounted responsive_secondrow]").text();
                                 info.setText(title + " " + price + " " + discountedPrice);
-                            }*/
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        wrongName.setText("Wrong title ?");
+                                        wrongName.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                info.setText(" ");
+                                                Elements el2 = doc.select("#search_results");
+                                                Elements elements = el2.select("a");
+                                                boolean found = false;
+                                                for (Element element : elements){
+                                                    String title = element.select("span.title").text();
+                                                    if (name.toUpperCase().equals(title.toUpperCase())){
+                                                        String price = element.select("div[class=col search_price  responsive_secondrow]").text();
+                                                        String discountedPrice = element.select("div[class=col search_price discounted responsive_secondrow]").text();
+                                                        info.setText(title + " " + price + " " + discountedPrice);
+                                                        found = true;
+                                                        wrongName.setText("");
+                                                    }
+                                                }
+                                                if (found == false){
+                                                    info.setText("can't find game with this title");
+                                                    wrongName.setText("");
+                                                }
+                                            }
+                                        });
+                                    }
+                                });
+                            }
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
